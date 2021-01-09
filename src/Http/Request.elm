1d74ffc562672f83dd91c5fb87cee4fcfcf7a5c6
Fabien Henon <henon.fabien@softcreations.fr>
2021-01-09 12:43:23 +0100

    Add extended requests that allows to extract some headers

--- src/Http/Request.elm
+++ src/Http/Request.elm
@@ -1,6 +1,6 @@
 module Http.Request exposing
-    ( Request, RequestCustomData, RequestNoContent, RequestFiles
-    , request, requestCustomData, requestNoContent, requestFiles
+    ( Request, RequestCustomData, RequestNoContent, RequestFiles, RequestEx, RequestCustomDataEx, RequestNoContentEx, RequestFilesEx
+    , request, requestCustomData, requestNoContent, requestFiles, requestEx, requestCustomDataEx, requestNoContentEx, requestFilesEx
     )
 
 {-| `Http.Request` allows you to create http requests with [`jsonapi`](https://package.elm-lang.org/packages/FabienHenon/jsonapi/latest/) objects.
@@ -11,12 +11,12 @@ The `content-type` used in the request headers is `application/vnd.api+json` acc
 
 # Definitions
 
-@docs Request, RequestCustomData, RequestNoContent, RequestFiles
+@docs Request, RequestCustomData, RequestNoContent, RequestFiles, RequestEx, RequestCustomDataEx, RequestNoContentEx, RequestFilesEx
 
 
 # Requests
 
-@docs request, requestCustomData, requestNoContent, requestFiles
+@docs request, requestCustomData, requestNoContent, requestFiles, requestEx, requestCustomDataEx, requestNoContentEx, requestFilesEx
 
 -}
 
@@ -45,6 +45,20 @@ type alias Request meta data =
     }
 
 
+{-| Defines a `RequestEx` with a url, some headers, a body, and a jsonapi document decoder.
+`meta`and `data` are types used by the jsonapi document that represent the `meta` object you would like to decode, and the `data` object you would
+like to decode.
+You can also extract some response headers to use them later
+-}
+type alias RequestEx meta data =
+    { url : Http.Url.Url
+    , headers : List Http.Header
+    , body : JE.Value
+    , documentDecoder : JD.Decoder (Result (List Decode.Error) (Document meta data))
+    , extractHeaders : List String
+    }
+
+
 {-| Defines a `Request` with a url, some headers, a body, and a custom decoder.
 `data` is the type of the object you would like to decode.
 -}
@@ -53,6 +67,19 @@ type alias RequestCustomData data =
     , headers : List Http.Header
     , body : JE.Value
     , documentDecoder : JD.Decoder (Result (List Decode.Error) data)
+    }
+
+
+{-| Defines a `Request` with a url, some headers, a body, and a custom decoder.
+`data` is the type of the object you would like to decode.
+You can also extract some response headers to use them later
+-}
+type alias RequestCustomDataEx data =
+    { url : Http.Url.Url
+    , headers : List Http.Header
+    , body : JE.Value
+    , documentDecoder : JD.Decoder (Result (List Decode.Error) data)
+    , extractHeaders : List String
     }
 
 
@@ -71,6 +98,23 @@ type alias RequestFiles meta data msg =
     }
 
 
+{-| Defines a `Request` with a url, some headers, a list of files, and a custom decoder.
+`data` is the type of the object you would like to decode.
+You can also extract some response headers to use them later
+-}
+type alias RequestFilesEx meta data msg =
+    { url : Http.Url.Url
+    , headers : List Http.Header
+    , multipartNames : List String
+    , files : List File
+    , multipartEncoder : String -> File -> Http.Part
+    , documentDecoder : JD.Decoder (Result (List Decode.Error) (Document meta data))
+    , tracker : Maybe String
+    , extractHeaders : List String
+    , msg : RemoteData.RemoteData ( RequestError, List ( String, String ) ) ( Document meta data, List ( String, String ) ) -> msg
+    }
+
+
 {-| Defines a `Request` with a url, some headers, and a body. There is no content decoder.
 Useful for `DELETE` requests for instance.
 -}
@@ -78,6 +122,18 @@ type alias RequestNoContent =
     { url : Http.Url.Url
     , headers : List Http.Header
     , body : JE.Value
+    }
+
+
+{-| Defines a `Request` with a url, some headers, and a body. There is no content decoder.
+Useful for `DELETE` requests for instance.
+You can also extract some response headers to use them later
+-}
+type alias RequestNoContentEx =
+    { url : Http.Url.Url
+    , headers : List Http.Header
+    , body : JE.Value
+    , extractHeaders : List String
     }
 
 
@@ -92,7 +148,23 @@ request request_ =
             Http.header "Accept" "application/vnd.api+json"
                 :: request_.headers
         , body = Http.stringBody "application/vnd.api+json" (JE.encode 0 request_.body)
-        , resolver = resourceResolver request_.documentDecoder
+        , resolver = resourceResolver request_.documentDecoder [] removeHeadersFromTask
+        , timeout = Nothing
+        }
+
+
+{-| Create a request task that will decode a jsonapi object. It will also return in the msg the list of extracted headers.
+-}
+requestEx : RequestEx meta data -> Task Never (RemoteData.RemoteData ( RequestError, List ( String, String ) ) ( Document meta data, List ( String, String ) ))
+requestEx request_ =
+    Http.task
+        { method = request_.url |> .method |> Http.Methods.toString
+        , url = request_.url |> .url
+        , headers =
+            Http.header "Accept" "application/vnd.api+json"
+                :: request_.headers
+        , body = Http.stringBody "application/vnd.api+json" (JE.encode 0 request_.body)
+        , resolver = resourceResolver request_.documentDecoder request_.extractHeaders identity
         , timeout = Nothing
         }
 
@@ -108,7 +180,23 @@ requestCustomData request_ =
             Http.header "Accept" "application/vnd.api+json"
                 :: request_.headers
         , body = Http.stringBody "application/vnd.api+json" (JE.encode 0 request_.body)
-        , resolver = resourceResolver request_.documentDecoder
+        , resolver = resourceResolver request_.documentDecoder [] removeHeadersFromTask
+        , timeout = Nothing
+        }
+
+
+{-| Create a request task that will decode a custom object. It will also return in the msg the list of extracted headers.
+-}
+requestCustomDataEx : RequestCustomDataEx data -> Task Never (RemoteData.RemoteData ( RequestError, List ( String, String ) ) ( data, List ( String, String ) ))
+requestCustomDataEx request_ =
+    Http.task
+        { method = request_.url |> .method |> Http.Methods.toString
+        , url = request_.url |> .url
+        , headers =
+            Http.header "Accept" "application/vnd.api+json"
+                :: request_.headers
+        , body = Http.stringBody "application/vnd.api+json" (JE.encode 0 request_.body)
+        , resolver = resourceResolver request_.documentDecoder request_.extractHeaders identity
         , timeout = Nothing
         }
 
@@ -124,7 +212,24 @@ requestFiles request_ =
                 :: request_.headers
         , url = request_.url |> .url
         , body = List.map2 request_.multipartEncoder request_.multipartNames request_.files |> Http.multipartBody
-        , expect = resourceExpecter request_.msg request_.documentDecoder
+        , expect = resourceExpecter (removeHeadersFromTask >> request_.msg) request_.documentDecoder []
+        , timeout = Nothing
+        , tracker = request_.tracker
+        }
+
+
+{-| Create a request files that will decode a jsonapi object. It will also return in the msg the list of extracted headers.
+-}
+requestFilesEx : RequestFilesEx meta data msg -> Cmd msg
+requestFilesEx request_ =
+    Http.request
+        { method = request_.url |> .method |> Http.Methods.toString
+        , headers =
+            Http.header "Accept" "application/vnd.api+json"
+                :: request_.headers
+        , url = request_.url |> .url
+        , body = List.map2 request_.multipartEncoder request_.multipartNames request_.files |> Http.multipartBody
+        , expect = resourceExpecter request_.msg request_.documentDecoder request_.extractHeaders
         , timeout = Nothing
         , tracker = request_.tracker
         }
@@ -141,24 +246,40 @@ requestNoContent request_ =
             Http.header "Accept" "application/vnd.api+json"
                 :: request_.headers
         , body = Http.stringBody "application/vnd.api+json" (JE.encode 0 request_.body)
-        , resolver = noContentResolver
+        , resolver = noContentResolver [] removeHeadersFromNoContentTask
         , timeout = Nothing
         }
 
 
-resourceExpecter : (RemoteData.RemoteData RequestError (Document meta data) -> msg) -> JD.Decoder (Result (List Decode.Error) (Document meta data)) -> Http.Expect msg
-resourceExpecter msg documentDecoder =
-    Http.expectStringResponse (Result.withDefault (RemoteData.Failure (CustomError "Unknown error")) >> msg) <|
+{-| Create a request task that will expect nothing to decode. It will also return in the msg the list of extracted headers.
+-}
+requestNoContentEx : RequestNoContentEx -> Task Never (RemoteData.RemoteData ( RequestError, List ( String, String ) ) (List ( String, String )))
+requestNoContentEx request_ =
+    Http.task
+        { method = request_.url |> .method |> Http.Methods.toString
+        , url = request_.url |> .url
+        , headers =
+            Http.header "Accept" "application/vnd.api+json"
+                :: request_.headers
+        , body = Http.stringBody "application/vnd.api+json" (JE.encode 0 request_.body)
+        , resolver = noContentResolver request_.extractHeaders identity
+        , timeout = Nothing
+        }
+
+
+resourceExpecter : (RemoteData.RemoteData ( RequestError, List ( String, String ) ) ( Document meta data, List ( String, String ) ) -> msg) -> JD.Decoder (Result (List Decode.Error) (Document meta data)) -> List String -> Http.Expect msg
+resourceExpecter msg documentDecoder headersToExtract =
+    Http.expectStringResponse (Result.withDefault (RemoteData.Failure ( CustomError "Unknown error", [] )) >> msg) <|
         \response ->
             case response of
                 Http.BadUrl_ url ->
-                    Ok (RemoteData.Failure (HttpError (Http.BadUrl url)))
+                    Ok (RemoteData.Failure ( HttpError (Http.BadUrl url), [] ))
 
                 Http.Timeout_ ->
-                    Ok (RemoteData.Failure (HttpError Http.Timeout))
+                    Ok (RemoteData.Failure ( HttpError Http.Timeout, [] ))
 
                 Http.NetworkError_ ->
-                    Ok (RemoteData.Failure (HttpError Http.NetworkError))
+                    Ok (RemoteData.Failure ( HttpError Http.NetworkError, [] ))
 
                 Http.BadStatus_ metadata body ->
                     if metadata.statusCode == 422 then
@@ -166,44 +287,44 @@ resourceExpecter msg documentDecoder =
                             Ok document ->
                                 case document of
                                     Err errors ->
-                                        Ok (RemoteData.Failure (JsonApiError errors))
+                                        Ok (RemoteData.Failure ( JsonApiError errors, extractHeaders headersToExtract metadata ))
 
                                     Ok doc ->
-                                        Ok (RemoteData.Success doc)
+                                        Ok (RemoteData.Success ( doc, extractHeaders headersToExtract metadata ))
 
                             Err err ->
-                                Ok (RemoteData.Failure (HttpError (Http.BadBody (JD.errorToString err))))
+                                Ok (RemoteData.Failure ( HttpError (Http.BadBody (JD.errorToString err)), extractHeaders headersToExtract metadata ))
 
                     else
-                        Ok (RemoteData.Failure (HttpError (Http.BadStatus metadata.statusCode)))
+                        Ok (RemoteData.Failure ( HttpError (Http.BadStatus metadata.statusCode), extractHeaders headersToExtract metadata ))
 
                 Http.GoodStatus_ metadata body ->
                     case JD.decodeString documentDecoder body of
                         Ok document ->
                             case document of
                                 Err errors ->
-                                    Ok (RemoteData.Failure (JsonApiError errors))
+                                    Ok (RemoteData.Failure ( JsonApiError errors, extractHeaders headersToExtract metadata ))
 
                                 Ok doc ->
-                                    Ok (RemoteData.Success doc)
+                                    Ok (RemoteData.Success ( doc, extractHeaders headersToExtract metadata ))
 
                         Err err ->
-                            Ok (RemoteData.Failure (HttpError (Http.BadBody (JD.errorToString err))))
+                            Ok (RemoteData.Failure ( HttpError (Http.BadBody (JD.errorToString err)), extractHeaders headersToExtract metadata ))
 
 
-resourceResolver : JD.Decoder (Result (List Decode.Error) a) -> Http.Resolver Never (RemoteData.RemoteData RequestError a)
-resourceResolver documentDecoder =
+resourceResolver : JD.Decoder (Result (List Decode.Error) a) -> List String -> (RemoteData.RemoteData ( RequestError, List ( String, String ) ) ( a, List ( String, String ) ) -> RemoteData.RemoteData err succ) -> Http.Resolver Never (RemoteData.RemoteData err succ)
+resourceResolver documentDecoder headersToExtract mapper =
     Http.stringResolver <|
         \response ->
-            case response of
+            (case response of
                 Http.BadUrl_ url ->
-                    Ok (RemoteData.Failure (HttpError (Http.BadUrl url)))
+                    Ok (RemoteData.Failure ( HttpError (Http.BadUrl url), [] ))
 
                 Http.Timeout_ ->
-                    Ok (RemoteData.Failure (HttpError Http.Timeout))
+                    Ok (RemoteData.Failure ( HttpError Http.Timeout, [] ))
 
                 Http.NetworkError_ ->
-                    Ok (RemoteData.Failure (HttpError Http.NetworkError))
+                    Ok (RemoteData.Failure ( HttpError Http.NetworkError, [] ))
 
                 Http.BadStatus_ metadata body ->
                     if metadata.statusCode == 422 then
@@ -211,58 +332,77 @@ resourceResolver documentDecoder =
                             Ok document ->
                                 case document of
                                     Err errors ->
-                                        Ok (RemoteData.Failure (JsonApiError errors))
+                                        Ok (RemoteData.Failure ( JsonApiError errors, extractHeaders headersToExtract metadata ))
 
                                     Ok doc ->
-                                        Ok (RemoteData.Success doc)
+                                        Ok (RemoteData.Success ( doc, extractHeaders headersToExtract metadata ))
 
                             Err err ->
-                                Ok (RemoteData.Failure (HttpError (Http.BadBody (JD.errorToString err))))
+                                Ok (RemoteData.Failure ( HttpError (Http.BadBody (JD.errorToString err)), extractHeaders headersToExtract metadata ))
 
                     else
-                        Ok (RemoteData.Failure (HttpError (Http.BadStatus metadata.statusCode)))
+                        Ok (RemoteData.Failure ( HttpError (Http.BadStatus metadata.statusCode), extractHeaders headersToExtract metadata ))
 
                 Http.GoodStatus_ metadata body ->
                     case JD.decodeString documentDecoder body of
                         Ok document ->
                             case document of
                                 Err errors ->
-                                    Ok (RemoteData.Failure (JsonApiError errors))
+                                    Ok (RemoteData.Failure ( JsonApiError errors, extractHeaders headersToExtract metadata ))
 
                                 Ok doc ->
-                                    Ok (RemoteData.Success doc)
+                                    Ok (RemoteData.Success ( doc, extractHeaders headersToExtract metadata ))
 
                         Err err ->
-                            Ok (RemoteData.Failure (HttpError (Http.BadBody (JD.errorToString err))))
+                            Ok (RemoteData.Failure ( HttpError (Http.BadBody (JD.errorToString err)), extractHeaders headersToExtract metadata ))
+            )
+                |> Result.map mapper
 
 
-noContentResolver : Http.Resolver Never (RemoteData.RemoteData RequestError ())
-noContentResolver =
+noContentResolver : List String -> (RemoteData.RemoteData ( RequestError, List ( String, String ) ) (List ( String, String )) -> RemoteData.RemoteData err succ) -> Http.Resolver Never (RemoteData.RemoteData err succ)
+noContentResolver headersToExtract mapper =
     Http.stringResolver <|
         \response ->
-            case response of
+            (case response of
                 Http.BadUrl_ url ->
-                    Ok (RemoteData.Failure (HttpError (Http.BadUrl url)))
+                    Ok (RemoteData.Failure ( HttpError (Http.BadUrl url), [] ))
 
                 Http.Timeout_ ->
-                    Ok (RemoteData.Failure (HttpError Http.Timeout))
+                    Ok (RemoteData.Failure ( HttpError Http.Timeout, [] ))
 
                 Http.NetworkError_ ->
-                    Ok (RemoteData.Failure (HttpError Http.NetworkError))
+                    Ok (RemoteData.Failure ( HttpError Http.NetworkError, [] ))
 
                 Http.BadStatus_ metadata body ->
-                    Ok (RemoteData.Failure (HttpError (Http.BadStatus metadata.statusCode)))
+                    Ok (RemoteData.Failure ( HttpError (Http.BadStatus metadata.statusCode), extractHeaders headersToExtract metadata ))
 
                 Http.GoodStatus_ metadata body ->
                     case metadata.statusCode of
                         200 ->
-                            Ok (RemoteData.Success ())
+                            Ok (RemoteData.Success (extractHeaders headersToExtract metadata))
 
                         202 ->
-                            Ok (RemoteData.Success ())
+                            Ok (RemoteData.Success (extractHeaders headersToExtract metadata))
 
                         204 ->
-                            Ok (RemoteData.Success ())
+                            Ok (RemoteData.Success (extractHeaders headersToExtract metadata))
 
                         _ ->
-                            Ok (RemoteData.Failure (HttpError (Http.BadStatus metadata.statusCode)))
+                            Ok (RemoteData.Failure ( HttpError (Http.BadStatus metadata.statusCode), extractHeaders headersToExtract metadata ))
+            )
+                |> Result.map mapper
+
+
+extractHeaders : List String -> Http.Metadata -> List ( String, String )
+extractHeaders headersToExtract metadata =
+    []
+
+
+removeHeadersFromTask : RemoteData.RemoteData ( err, List ( String, String ) ) ( succ, List ( String, String ) ) -> RemoteData.RemoteData err succ
+removeHeadersFromTask =
+    RemoteData.mapBoth Tuple.first Tuple.first
+
+
+removeHeadersFromNoContentTask : RemoteData.RemoteData ( err, List ( String, String ) ) (List ( String, String )) -> RemoteData.RemoteData err ()
+removeHeadersFromNoContentTask =
+    RemoteData.mapBoth (always ()) Tuple.first
